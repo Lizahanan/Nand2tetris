bd96f2d2b266f7dc90dcb6930017facf5d9054a5
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-03-03 20:02:44 +0200

    Parser impelemented

--- Project7/python/Parser.py
+++ Project7/python/Parser.py
@@ -30,29 +30,48 @@ class Parser:
         '''
         Returns true if there are more commands in the input.
         '''
-        pass
+        return self.current_index < len(self.commands)
 
     def advance(self):
         '''
         Reads the next command from the input and makes it the current command.
         '''
-        pass
+        #check if there are more commands
+        if self.hasMoreCommands():
+            self.current_command = self.commands[self.current_index]
+            self.current_index += 1
+        
 
     def commandType(self):
         '''
         Returns the type of the current command.
         '''
-        pass
+        if self.current_command in ["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]:
+            return self.C_ARITHMETIC
+        elif self.current_command.startswith("push"):
+            return self.C_PUSH
+        elif self.current_command.startswith("pop"):
+            return self.C_POP
+        else:
+            raise ValueError("Invalid command: {}".format(self.current_command))
 
     def arg1(self):
         '''returns first argument of the current command'''
-        pass
+        #if the command is arithmetic, return the command itself
+        if self.commandType() == self.C_ARITHMETIC:
+            return self.current_command
+        #otherwise, return the first argument
+        return self.current_command.split()[1] 
 
     def arg2(self):
         '''
         Returns the second argument of the current command.
         '''
-        pass
+        #only push and pop commands have a second argument
+        if self.commandType() in [self.C_PUSH, self.C_POP]:
+            return int(self.current_command.split()[2])
+        else:
+            raise ValueError(f"Current command {self.current_command} of type {self.commandType()} does not have a second argument")
 
     def close(self):
         ''' closes the file'''

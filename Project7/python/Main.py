f7c3a4c1879c4e464242a56d4ee1ad12acfa3a90
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-03-03 19:52:32 +0200

    initial commit

---
+++ Project7/python/Main.py
@@ -0,0 +1,14 @@
+'''The driving script for the program. 
+This script will be used to run the program and call the necessary functions to run the program.
+In simple words the engine of our VM translator.
+'''
+import sys
+from Parser import Parser
+from CodeWriter import CodeWriter
+import os
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

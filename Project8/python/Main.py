c9e837d3d2f9d6831942375b3d753a8e3d69991e
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-03-07 21:07:53 +0200

    main

--- Project8/python/Main.py
+++ Project8/python/Main.py
@@ -5,36 +5,41 @@ In simple words the engine of our VM translator.
 import sys
 from Parser import Parser
 from CodeWriter import CodeWriter
-import os
 
 def main():
-    #check if the user has provided the correct number of arguments
     if len(sys.argv) != 2:
-        print("Usage: python Main.py <file_name>")
+        print("Usage: VMTranslator <inputfile.vm or directory>")
         sys.exit(1)
-
-    #get the file name from the command line argument
-    input_file = sys.argv[1]
-    output_file = input_file.replace('.vm', '.asm') #create the output file name
-    file_name = input_file.split('/')[-1].replace('.vm', '') #get the file name without the path and extension
-
-    parser = Parser(input_file) #create a parser object
-    code_writer = CodeWriter(output_file, file_name) #create a code writer object
-
-    while parser.hasMoreCommands():
-        parser.advance()
-        command_type = parser.commandType()
-        if command_type == 'C_ARITHMETIC':
-            command = parser.arg1()
-            code_writer.writeArithmetic(command)
-        elif command_type in ['C_PUSH', 'C_POP']:
-            segment = parser.arg1()
-            index = parser.arg2()
-            code_writer.writePushPop(command_type, segment, index)
-        #the other commands are implemented in the next project
-        code_writer.close() #close the output file
-
     
+    input_path = sys.argv[1]
+    
+    # For simplicity, assume the input is a single .vm file.
+    # (You can add directory handling to process multiple files.)
+    parser = Parser(input_path)
+    output_file = input_path.replace('.vm', '.asm')
+    code_writer = CodeWriter(output_file)
+    
+    while parser.hasMoreCommands():
+        ctype = parser.commandType()
+        if ctype == Parser.C_ARITHMETIC:
+            code_writer.writeArithmetic(parser.arg1())
+        elif ctype in (Parser.C_PUSH, Parser.C_POP):
+            code_writer.writePushPop(ctype, parser.arg1(), parser.arg2())
+        elif ctype == Parser.C_LABEL:
+            code_writer.writeLabel(parser.arg1())
+        elif ctype == Parser.C_GOTO:
+            code_writer.writeGoto(parser.arg1())
+        elif ctype == Parser.C_IF:
+            code_writer.writeIf(parser.arg1())
+        elif ctype == Parser.C_FUNCTION:
+            code_writer.writeFunction(parser.arg1(), parser.arg2())
+        elif ctype == Parser.C_CALL:
+            code_writer.writeCall(parser.arg1(), parser.arg2())
+        elif ctype == Parser.C_RETURN:
+            code_writer.writeReturn()
+        parser.advance()
+
+    code_writer.close()
 
 if __name__ == "__main__":
     main()
